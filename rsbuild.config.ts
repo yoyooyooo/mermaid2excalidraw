93048e6d3f4efb46010cb25cfbb8cd811b85c7c0
Jackie.hu <jackie.hu@imile.me>
2024-12-21 19:11:39 +0800

    chore: update rsbuild config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,5 +3,8 @@ import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 
 export default defineConfig({
+  html: {
+    title: "Mermaid to Excalidraw",
+  },
   plugins: [pluginReact(), pluginNodePolyfill()],
 });
--- src/lib/process-polyfill.ts
+++
@@ -1,7 +0,0 @@
-if (typeof window !== "undefined" && !window.process) {
-  window.process = {
-    env: {
-      NODE_ENV: process.env.NODE_ENV || "development",
-    },
-  } as any;
-}
